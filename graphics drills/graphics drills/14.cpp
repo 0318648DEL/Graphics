b281700ccf50c4f39a90869e139623fbd652fa1a
0318648DEL <gleldzm321@gmail.com>
2018-10-03 05:30:44 +0900

    14완

--- graphics drills/graphics drills/14.cpp
+++ graphics drills/graphics drills/14.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <gl/freeglut.h>
 
 #define WIDTH 800
@@ -12,8 +13,10 @@ void keyBoard(unsigned char key, int x, int y);
 void Mouse(int button, int state, int x, int y);
 void Timer(int value);
 
-int num = 0;
-int trigger[20] = 0;
+int state = 1;
+int start_num = 0;
+int last_num = 20;
+int trigger[20] = { 0, };
 int shape_num[20] = { 0, };
 GLfloat coordinate[20] = { 0, };
 int rotate_axis[20] = { 0, };
@@ -30,14 +33,16 @@ int color3[20] = { 0, };
 
 int main(int argc, char **argv)
 {
+	srand((unsigned)time(NULL));
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(WIDTH, HEIGHT);
-	glutCreateWindow("Practice13");
+	glutCreateWindow("Practice14");
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(reShape);
 	glutMouseFunc(Mouse);
+	glutKeyboardFunc(keyBoard);
 	glutTimerFunc(100, Timer, 1);
 	glutMainLoop();
 }
@@ -45,10 +50,41 @@ int main(int argc, char **argv)
 GLvoid drawScene(GLvoid)
 {
 	int draw = 0;
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
-
+	for (int x = 19; x>=0; --x)
+	{
+		if (trigger[x] == 1)
+		{
+			glPushMatrix();
+			glColor4ub(color1[x], color2[x], color3[x], 1.0);
+			glTranslatef(trans_coordinate_x[x], trans_coordinate_y[x], 0.0f);
+			glRotatef(rotation[x], axis_x[x], axis_y[x], axis_z[x]);
+			glScalef(scale[x], scale[x], scale[x]);
+			switch (shape_num[x])
+			{
+			case 1:
+				glutWireCone(coordinate[x], coordinate[x], 8, 8);
+				break;
+			case 2:
+				glutWireCube(coordinate[x]);
+				break;
+			case 3:
+				glutWireSphere(coordinate[x] / 2, 10, 10);
+				break;
+			case 4:
+				glutWireTorus(coordinate[x] / 5, coordinate[x] / 2, 10, 10);
+				break;
+			case 5:
+				glutWireTeapot(coordinate[x]);
+				break;
+			}
+			glPopMatrix();
+		}
+		
+	}
+	glutSwapBuffers();
 }
 
 GLvoid reShape(int w, int h)
@@ -57,88 +93,100 @@ GLvoid reShape(int w, int h)
 	glOrtho(-400.0f, 400.0f, -300.0f, 300.0f, -400.0f, 400.0f);
 }
 
+void keyBoard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+	case 'r':
+		state *= -1;
+		break;
+	}
+}
+
 void Mouse(int button, int state, int x, int y)
 {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
-		if (num < 20)
+		if (start_num<last_num)
 		{
-			trigger[num] = 1;
-			trans_coordinate_x[num] = x;
-			trans_coordinate_y[num] = y;
-			coordinate[num] = 20.0f;
-			shape_num[num] = (rand() % 5) + 1;
-			scale[num] = (rand() % 10) + 5;
-			color1[num] = (rand() % 255) + 1;
-			color2[num] = (rand() % 255) + 1;
-			color3[num] = (rand() % 255) + 1;
-			rotate_axis[num] = rand() % 3;
-			switch (rotate_axis[num])
+			trigger[start_num] = 1;
+			trans_coordinate_x[start_num] = x-400;
+			trans_coordinate_y[start_num] = y-300;
+			coordinate[start_num] = (rand()%20)+5;
+			shape_num[start_num] = (rand() % 5) + 1;
+			scale[start_num] = 20;
+			color1[start_num] = (rand() % 255) + 1;
+			color2[start_num] = (rand() % 255) + 1;
+			color3[start_num] = (rand() % 255) + 1;
+			rotate_axis[start_num] = rand() % 3;
+			switch (rotate_axis[start_num])
 			{
 			case 0:
-				axis_x[num] = 1;
-				axis_y[num] = 0;
-				axis_z[num] = 0;
+				axis_x[start_num] = 1;
+				axis_y[start_num] = 0;
+				axis_z[start_num] = 0;
 				break;
 			case 1:
-				axis_x[num] = 0;
-				axis_y[num] = 1;
-				axis_z[num] = 0;
+				axis_x[start_num] = 0;
+				axis_y[start_num] = 1;
+				axis_z[start_num] = 0;
 				break;
 			case 2:
-				axis_x[num] = 0;
-				axis_y[num] = 0;
-				axis_z[num] = 1;
+				axis_x[start_num] = 0;
+				axis_y[start_num] = 0;
+				axis_z[start_num] = 1;
 				break;
 			}
-			++num;
+			++start_num;
+			start_num = start_num % last_num;
 		}
 	}
 }
 
 void Timer(int value)
 {
-	int temp = 0;
-	while (temp < 20)
+	int temp = 19;
+	while (temp >= 0)
 	{
 			if (scale[temp] <= 0&&trigger[temp]==1)
 			{
-				for (int i = temp; i < 19; ++i)
-				{
-					trigger[temp] = trigger[temp + 1];
-					shape_num[temp] = shape_num[temp+1];
-					scale[temp] = shape_num[temp+1];
-					color1[temp] = shape_num[temp + 1];
-					color2[temp] = shape_num[temp + 1];
-					color3[temp] = shape_num[temp + 1];
-					rotate_axis[temp] = rotate_axis[temp + 1];
-					axis_x[temp] = axis_x[temp + 1];
-					axis_y[temp] = axis_y[temp + 1];
-					axis_z[temp] = axis_z[temp + 1];
-					rotation[temp] = rotation[temp + 1];
-					coordinate[temp] = coordinate[temp + 1];
-					trans_coordinate_x[temp] = trans_coordinate_x[temp + 1];
-					trans_coordinate_y[temp] = trans_coordinate_y[temp + 1];
-				}
-				num = 19;
-				trigger[num] = 0;
+				trigger[temp] = 0;
+				/*shape_num[temp] = 0;
+				scale[temp] = 0;
+				color1[temp] =0;
+				color2[temp] =0;
+				color3[temp] =0;
+				rotate_axis[temp] = 0;
+				axis_x[temp] = 0;
+				axis_y[temp] = 0;
+				axis_z[temp] = 0;
+				rotation[temp] = 0;
+				coordinate[temp] = 0;
+				trans_coordinate_x[temp] = 0;
+				trans_coordinate_y[temp] = 0;*/
 			}
-			else
+			else if(trigger[temp]==1)
 			{
 				--scale[temp];
 				switch (rotate_axis[temp])
 				{
 				case 0:
-					rotation[temp] += axis_x[temp] * 20;
+					rotation[temp] += state*axis_x[temp] * 20;
 					break;
 				case 1:
-					rotation[temp] += axis_y[temp] * 20;
+					rotation[temp] += state*axis_y[temp] * 20;
 					break;
 				case 2:
-					rotation[temp] += axis_z[temp] * 20;
+					rotation[temp] += state*axis_z[temp] * 20;
 					break;
 				}
 			}
+			--temp;
 	}
-	
+	glutPostRedisplay();
+	glutTimerFunc(100, Timer, 1);
+
 }
---
+++ graphics drills/graphics drills/15.cpp
@@ -0,0 +1,66 @@
+#include "pch.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <gl/freeglut.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+GLvoid drawScene(GLvoid);
+GLvoid reShape(int w, int h);
+void Timer(int value);
+
+struct number
+{
+
+};
+int hour = 0;
+int minute = 0;
+int second = 0;
+
+int main(int argc, char **argv)
+{
+	srand((unsigned)time(NULL));
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Practice14");
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(reShape);
+	glutTimerFunc(100, Timer, 1);
+	glutMainLoop();
+}
+
+GLvoid drawScene()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+GLvoid reShape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glOrtho(-400.0f, 400.0f, -300.0f, 300.0f, -400.0f, 400.0f);
+}
+
+void get_time()
+{
+	time_t timer;
+	struct tm *t;
+
+	timer = time(NULL);
+
+	t = localtime(&timer);
+
+	hour = t->tm_hour;
+	minute = t->tm_min;
+	second = t->tm_sec;
+}
+
+void Timer(int value)
+{
+	get_time();
+}
